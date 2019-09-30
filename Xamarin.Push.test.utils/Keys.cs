5c16527fd1acd3ad82454856ed76a1f56353ad79
RagibNoor <ragibnoor19@gmail.com>
2019-09-30 13:35:56 +0600

    add appcenter notification package

---
+++ Xamarin.Push.test.utils/Keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Xamarin.Push.test.utils
+{
+    public class Keys
+    {
+        //auth
+        public const string AppCenterApiKey = "";
+        // vs app center app secret
+        public const string AndroidAppCenterSecret = "";
+        public const string IosAppCenterSecret = "";
+        public const string OwnerName = "";
+        public const string AndroidAppName = "";
+        public const string IosAppName = "";
+    }
+}
